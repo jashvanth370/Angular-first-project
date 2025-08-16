e2e08d7170d4c6a32b536f94ae7863804a6398cb
jashvanth370 <jashjazz11@gmail.com>
2025-08-16 18:04:09 +0530

    App component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DemoComponent,CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'counter-app';
+  onCarAdded(carName:string){
+    console.log(carName,'from app')
+  }
 }
