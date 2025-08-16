0e88cf8f8275b50469347c41f413bd76e04353e2
jashvanth370 <jashjazz11@gmail.com>
2025-08-16 18:03:52 +0530

    counter component created

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
+  count=0;
+  increment() {
+    this.count++;
+  }
 
 }
